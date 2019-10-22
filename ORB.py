ba4c75364e8d0e843fec5a89e8b84477698bac28
SALİH BESKARDES <32905535+sweetnatured@users.noreply.github.com>
2019-10-22 15:43:15 +0300

    Update ORB.py

--- ORB.py
+++ ORB.py
@@ -1,15 +1,16 @@
 import cv2
 import numpy as np
 
-image=cv2.imread('C:/Users/SALIH/Desktop/Codes/Python/ComputerVision/Triangle.jpg',0)
+image=cv2.imread('C:/Users/SALIH/Desktop/Codes/Python/ComputerVision/truck.jpg',0)
+resized = cv2.resize(image, (1000,500), interpolation = cv2.INTER_AREA)
 
-orb = cv2.ORB(1000)
+orb = cv2.ORB_create(nfeatures=100)
 
-keypoints=orb.detect(image,None)
+keypoints=orb.detect(resized,None)
 
-keypoints,descriptors=orb.compute(image,keypoints)
+keypoints,descriptors=orb.compute(resized,keypoints)
 
-imag=cv2.drawKeypoints(image,keypoints,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+imag=cv2.drawKeypoints(resized,keypoints,outImage=None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 cv2.imshow('ORB',imag)
 cv2.waitKey(0)
